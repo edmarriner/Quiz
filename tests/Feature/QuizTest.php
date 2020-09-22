b9c015c504f972cdf6d9b9f60a54a4e3f927ad13
edmarriner <edmarriner@gmail.com>
2020-09-22 21:35:26 +0100

    Added test for listing quizzes

--- tests/Feature/QuizTest.php
+++ tests/Feature/QuizTest.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Quiz;
 use App\User;
 use Laravel\Passport\Passport;
+use function Pest\Laravel\getJson;
 use function Pest\Laravel\postJson;
 
 test('can create a new quiz', function(){
@@ -36,3 +38,37 @@ test('fails to create test if not all required fields present', function(){
         ->assertStatus(422);
 });
 
+test('can list quizzes', function(){
+
+    Passport::actingAs(User::factory()->create(), [
+        'create-quiz'
+    ]);
+
+    Quiz::insert([
+        ['name' => 'test quiz']
+    ]);
+
+    $testResponse = getJson('/api/quizzes', []);
+
+    $testResponse
+        ->assertJson([
+            [ 'name' => 'test quiz' ]
+        ]);
+});
+
+test('can filter quizzes by name', function(){
+
+    Passport::actingAs(User::factory()->create(), [
+        'create-quiz'
+    ]);
+
+    Quiz::insert([
+        ['name' => 'test quiz'],
+        ['name' => 'sample'],
+    ]);
+
+    $testResponse = getJson('/api/quizzes?filter[name]=sample', []);
+
+    assert(sizeof($testResponse->json()) === 1);
+
+});
